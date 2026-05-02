4719953e768b821c8727d50c2567fd46f890e723
IIIMAXIMIII <145893187+IIIMAXIMIII@users.noreply.github.com>
2026-05-02 17:17:51 +0500

    fix svg

--- frontend/src/components/SocialIcons.tsx
+++ frontend/src/components/SocialIcons.tsx
@@ -94,8 +94,8 @@ export const TikTokIcon = (props: React.SVGProps<SVGSVGElement>) => (
 
 export const SpotifyIcon = (props: React.SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" id="spotify" {...props}>
-    <path stroke-width="36" d="m141 195c75-20 164-15 238 24"></path>
-    <path stroke-width="31" d="m152 257c61-17 144-13 203 24"></path>
-    <path stroke-width="24" d="m156 315c54-12 116-17 178 20"></path>
+    <path strokeWidth="36" d="m141 195c75-20 164-15 238 24"></path>
+    <path strokeWidth="31" d="m152 257c61-17 144-13 203 24"></path>
+    <path strokeWidth="24" d="m156 315c54-12 116-17 178 20"></path>
   </svg>
 );
